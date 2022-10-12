ee10426121007664a693489a2530120447e57f09
jazzman55 <jazzman5@poczta.onet.pl>
2022-10-12 13:48:56 +0200

    initial

---
+++ src/MakoIoT.Device.Utilities.String/Extensions/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿using System.Text;
+
+namespace MakoIoT.Device.Utilities.String.Extensions
+{
+    public static class StringExtension
+    {
+        /// <summary>
+        /// Escapes curly braces, so the string can be used for interpolation.
+        /// </summary>
+        /// <param name="s">The string</param>
+        /// <returns>Escaped string</returns>
+        public static string EscapeForInterpolation(this string s)
+        {
+            var builder = new StringBuilder(s);
+            builder.Replace("{", "{{").Replace("}", "}}");
+            return builder.ToString();
+        }
+    }
+}
